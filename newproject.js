5505b4354dc11fe187bfd3e6eb46f7abd63f700a
hbc8141 <ubseubse@UbSE-UbSEui-iMac.local>
2016-06-02 00:23:12 +0900

    android sms parse_meteor test
    안드로이드에서 각 카드사 별로 파싱된 문자를 json형식으로 변환한 후 meteor db에 추가함.

---
+++ newproject.js
@@ -0,0 +1,37 @@
+Tasks = new Mongo.Collection('tasks');
+
+parser = function(sms){
+  var list = JSON.parse(JSON.stringify(sms));
+  // json -> string 변환 후 배열화.
+
+  alert(list[0].place);
+  if(Tasks.find({}).count()!=0)
+    Tasks.remove();
+
+  for(var i=0;i<list.length;i++){
+    Tasks.insert({
+      bank : list[i].bank,
+      month : list[i].month,
+      day : list[i].day,
+      time : list[i].time,
+      place : list[i].place,
+      money : list[i].money
+    });
+  }
+} // android -> mongo
+
+if (Meteor.isClient) {
+
+  Template.hello.events({
+    'click button.a': function () {
+      // increment the counter when button is clicked
+      window.smsParser.setMessage("Android");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
